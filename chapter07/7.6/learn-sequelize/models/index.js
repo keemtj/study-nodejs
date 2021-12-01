4880351678b39132443441abb776d342af05b115
keemtj <keemgreat@gmail.com>
2021-12-01 19:19:29 +0900

    :bulb: Add comments

--- chapter07/7.6/learn-sequelize/models/index.js
+++ chapter07/7.6/learn-sequelize/models/index.js
@@ -6,13 +6,16 @@ const env = process.env.NODE_ENV || "development";
 const config = require("../config/config")[env];
 const db = {};
 
+// Sequelize 패키지이자 생성자이다.
+// config/config.json에서 데이터베이스 설정을 불러온 후
+// new Sequelize를 통해 MySQL 연결 객체를 생성한다.
 const sequelize = new Sequelize(
   config.database,
   config.username,
   config.password,
   config
 );
-
+// 연결 객체를 나중에 재사용하기 위해 db.sequelize에 넣어둔다.
 db.sequelize = sequelize;
 
 // db 객체에 User, Comment 모델을 담아두었다
